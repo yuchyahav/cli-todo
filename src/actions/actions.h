8b8dbc7d53d82595bee1976460fdef05c9cf798b
bmendbayar <btrglm@proton.me>
2026-01-02 19:06:54 -0700

    feat: api for undo/redo implemented

--- src/actions/actions.h
+++ src/actions/actions.h
@@ -9,9 +9,10 @@ namespace Todo {
 class Action {
 protected:
   Model *model_{nullptr};
-  std::vector<u16> path_;
+  std::vector<u16> exe_path_;
 
 public:
+  Action(Model &model, std::vector<u16> path);
   virtual ~Action() = default;
   virtual void execute() = 0;
   virtual void undo() = 0;
@@ -19,44 +20,45 @@ public:
 
 class RemoveAction : public Action {
 private:
+  std::vector<u16> undo_path_;
   Task task_;
 
 public:
-  RemoveAction(Model &model, Task task, std::vector<u16> path);
+  RemoveAction(Model &model, std::vector<u16> path, Task task);
   virtual void execute() override;
   virtual void undo() override;
 };
 
 class AddAction : public Action {
 private:
+  std::vector<u16> undo_path_;
   Task task_;
 
 public:
-  AddAction(Model &model, Task task, std::vector<u16> path);
+  AddAction(Model &model, std::vector<u16> path, Task task);
   virtual void execute() override;
   virtual void undo() override;
 };
 
 class StatusChangeAction : public Action {
 private:
-  Status new_st_;
-  Status old_st_;
+  Status new_status_;
+  Status old_status_;
 
 public:
-  StatusChangeAction(Model &model, Status new_st, Status old_st, std::vector<u16> path);
+  StatusChangeAction(Model &model, std::vector<u16> path, Status new_status);
   virtual void execute() override;
   virtual void undo() override;
 };
 
 class PriorityChangeAction : public Action {
 private:
-  u16 new_p_;
-  u16 old_p_;
+  u16 new_priority_;
+  u16 old_priority_;
 
 public:
-  PriorityChangeAction(Model &model, u16 new_p, u16 old_p, std::vector<u16> path);
+  PriorityChangeAction(Model &model, std::vector<u16> path, u16 new_priority);
   virtual void execute() override;
   virtual void undo() override;
 };
-
 }  // namespace Todo
